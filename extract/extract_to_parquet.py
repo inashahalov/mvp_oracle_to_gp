1d670900bce7efc39d525709ca3306278b08c9ec
inashahalov <nashahalov@outlook.com>
2026-02-02 10:42:36 +0300

    oracle - .parquet настроен oracle

--- extract/extract_to_parquet.py
+++ extract/extract_to_parquet.py
@@ -8,14 +8,13 @@ import pyarrow.parquet as pq
 dsn = oracledb.makedsn("localhost", 1521, service_name="XEPDB1")
 conn = oracledb.connect(user="appuser", password="AppPass123_", dsn=dsn)
 
-
 def oracle_to_parquet(table_name):
     df = pd.read_sql(f"SELECT * FROM {table_name}", conn)
 
     # Преобразование типов
     for col in df.columns:
         if df[col].dtype == 'object':
-            # CLOB приходит как str, но может быть None
+            # Обработка CLOB/NULL
             df[col] = df[col].astype(str).replace('None', None)
         elif 'date' in str(df[col].dtype).lower() or 'datetime' in str(df[col].dtype):
             df[col] = pd.to_datetime(df[col], errors='coerce')
@@ -25,8 +24,11 @@ def oracle_to_parquet(table_name):
     pq.write_table(table, f"{table_name}.parquet")
     print(f"✅ {table_name} → {table_name}.parquet")
 
-
 if __name__ == "__main__":
-    oracle_to_parquet("customers")
-    oracle_to_parquet("orders")
-    conn.close()
+    try:
+        # Исправленный список таблиц — только те, что есть в схеме
+        tables = ["customers", "accounts", "transactions", "audit_log"]
+        for tbl in tables:
+            oracle_to_parquet(tbl)
+    finally:
+        conn.close()
